040cf3d96dcd084ac67d597ef44bdbf059cb5b99
Matěj Gajdoš <gajdos.matej@yahoo.com>
2020-10-04 15:23:20 +0200

    [FEATURE] Pridany fce definiteIntegral(), getTemperatureDistribution() [ISSUE] Teplotni rozdeleni na zaklade Lagrangeova polynomu se v interpolovanych bodech skokove meni

--- map.js
+++ map.js
@@ -32,7 +32,10 @@ class Map{
 
         var x_arr = [0,10,20,30,40,50,60,70,80,90,-10,-20,-30,-40,-50,-60,-70,-80, -90];
         var y_arr = [27,26,25,21,15,7,3,0,-2,-3,26,25,21,15,7,3,0,-2,-3];
-        //window.alert((lagrangePolynomial(x_arr,y_arr))(-0.5));
+        //window.alert((lagrangePolynomial(x_arr,y_arr))(-86.4));
+        var f = getTemperatureDistribution(x_arr, y_arr, 10);
+        window.alert(f(90));
+
         var tile0 = new ForestTile(1,1);
         var width = tile0.width, height = tile0.height;
         tile0.delete();
--- math_functions.js
+++ math_functions.js
@@ -41,3 +41,36 @@ function lagrangePolynomial(x_array, y_array){
     //return final_function;
 
 }
+
+function definiteIntegral(func, x1, x2, steps){
+
+    if(x1 > x2){
+        console.log("ERROR: definiteIntegral --- wrong bounds of integral");
+        return;
+    }
+
+    var step = (x2 - x1) / steps;
+    var sum = 0;
+
+    for(var i = x1; i < x2; i += step){
+
+        sum += func(i) * step;
+        console.log("x: " + i + " f(x): " + func(i));
+
+    }
+
+    return sum;
+
+}
+
+
+function getTemperatureDistribution(latitude_array, temperature_array, T_average_demanded){
+
+    baseTemperatureDist = lagrangePolynomial(latitude_array, temperature_array);
+    base_T_average = definiteIntegral(baseTemperatureDist, -90, 90, 18) / 180;
+
+    temperatureDist = (x) => {  return baseTemperatureDist(x) + (T_average_demanded - base_T_average);  }
+
+    return temperatureDist;
+
+}
