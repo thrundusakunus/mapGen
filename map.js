66dbdcf0459d5a97b600ccec6a62cee988e24153
Matěj Gajdoš <gajdos.matej@yahoo.com>
2020-10-05 14:07:57 +0200

    [FEATURE] Pridan destructor Map [FEATURE] Nove generovane Map se predava uzivatelsky vstup

--- map.js
+++ map.js
@@ -1,6 +1,13 @@
 
 class Map{
     mapArray = [];
+    map_height = null;
+    island_n = null;
+    land_ratio = null;
+    average_height = null;
+    humidity = null;
+    indentation = null;
+    average_T = null;
 
 
     createTile(tileObject, row, column){
@@ -28,12 +35,42 @@ class Map{
     }
 
 
-    constructor(){
+    destroy(){
+
+        for(var i = 0; i < this.mapArray.length; ++i){
+            for(var j = 0; j < this.mapArray[i].length; ++j){
+
+                if(this.mapArray[i][j] != undefined){
+                    this.mapArray[i][j].delete();
+                }
+            }
+        }
+
+        this.mapArray = [];
+    }
+
+    setInputValues(input){
+
+        this.map_height = input[0];
+        this.island_n = input[1];
+        this.land_ratio = input[2];
+        this.average_height = input[3];
+        this.humidity = input[4];
+        this.indentation = input[5];
+        this.average_T = input[6];
+
+
+    }
+
+    constructor(inputs){
+
+        this.setInputValues(inputs);
 
         var x_arr = [0,10,20,30,40,50,60,70,80,90,-10,-20,-30,-40,-50,-60,-70,-80, -90];
         var y_arr = [27,26,25,21,15,7,3,0,-2,-3,26,25,21,15,7,3,0,-2,-3];
 
-        var f = getTemperatureDistribution(x_arr, y_arr, 10);
+        var f = getTemperatureDistribution(x_arr, y_arr, this.average_T);
+        window.alert(f(90));
 
 
         var tile0 = new ForestTile(1,1);
